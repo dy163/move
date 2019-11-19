fbbb7d8ed333905f7af16149a4ea8253b6012e14
dy <duyuan9191@163.com>
2019-11-19 14:22:05 +0800

    委托查询样式修改完成（进一步修改）

--- src/page/personal/components/entrust.vue
+++ src/page/personal/components/entrust.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="entrust">
-    <van-nav-bar title="成交查询" @click-left="$router.back()">
+    <van-nav-bar title="委托查询" @click-left="$router.back()">
       <van-icon name="arrow-left" slot="left" />
     </van-nav-bar>
     <div class="entrust-content">
@@ -12,46 +12,29 @@
           <van-col span="5" class="header-style">状态</van-col>
         </van-row>
       </div>
-      <div class="entrust-content-include">
-        <div class="entrust-content-table">
-          <div>
-            <van-row>
-              <van-col span="5">贵州茅台</van-col>
-              <van-col span="2">
-                <img src="@/assets/bill.png" />
-              </van-col>
-              <van-col span="5" class="table-money">30.21元</van-col>
-              <van-col span="7" class="entrust-content-big">1,600</van-col>
-            </van-row>
-            <van-row>
-              <van-col span="5">102931</van-col>
-              <van-col span="9">2017-10-05 10:22</van-col>
-              <van-col span="5" class="entrust-content-small">1,600</van-col>
-            </van-row>
+      <van-list>
+        <div class="entrust-content-include" v-for="item in list" :key="item.id">
+          <div class="entrust-content-table">
+            <div>
+              <van-row>
+                <van-col span="5">{{ item.title }}</van-col>
+                <van-col span="2">
+                  <img src="@/assets/bill.png? @/assets/bill.png:@/assets/sell.png" />
+                  <!-- ? <img src="@/assets/bill.png" />:<img src="@/assets/sell.png" /> -->
+                </van-col>
+                <van-col span="5" class="table-money">{{ item.money }}</van-col>
+                <van-col span="7" class="entrust-content-big">{{ item.number }}</van-col>
+              </van-row>
+              <van-row>
+                <van-col span="5">{{ item.identifier }}</van-col>
+                <van-col span="9">{{ item.timer }}</van-col>
+                <van-col span="5" class="entrust-content-small">{{ item.amount }}</van-col>
+              </van-row>
+            </div>
           </div>
+          <p class="entrust-content-state">已成</p>
         </div>
-        <p class="entrust-content-state">已成</p>
-      </div>
-      <div class="entrust-content-include">
-        <div class="entrust-content-table">
-          <div>
-            <van-row>
-              <van-col span="5">茅台</van-col>
-              <van-col span="2">
-                <img src="@/assets/sell.png" />
-              </van-col>
-              <van-col span="5" class="table-money">30.21元</van-col>
-              <van-col span="7" class="entrust-content-big">1,600</van-col>
-            </van-row>
-            <van-row>
-              <van-col span="5">102931</van-col>
-              <van-col span="9">2017-10-05 10:22</van-col>
-              <van-col span="5" class="entrust-content-small">1,600</van-col>
-            </van-row>
-          </div>
-        </div>
-        <p class="entrust-content-state">已成</p>
-      </div>
+      </van-list>
     </div>
   </div>
 </template>
@@ -60,7 +43,12 @@
 export default {
   name: "Entrust",
   data() {
-    return {};
+    return {
+      list: [
+        { title:'贵州茅台',money: '30.21元',number: '1,600',identifier: '102931',timer: '2017-10-05 10:22', amount: '1,600' },
+        { title:'茅台',money: '30.21元',number: '1,600',identifier: '102931',timer: '2017-10-05 10:22', amount: '1,600' }
+      ]
+    };
   },
   methods: {}
 };
@@ -87,7 +75,7 @@ export default {
     font-size: 14px;
     margin: 0 15px;
     padding: 15px 0;
-    border-bottom: 1px dotted rgba(124, 124, 130, 1);
+    border-bottom: 1px dotted #2E2E2E;
     color: rgba(255, 255, 255, 1);
     position: relative;
     .entrust-content-table {
